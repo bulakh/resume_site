3574b323f21cb1f104c2d85d27b255ec1d80ec19
bulakh <64536057+bulakh@users.noreply.github.com>
2021-02-08 16:22:57 +0300

    Исправляет ширину экрана, добавляет интерактивность

--- source/js/button-up.js
+++ source/js/button-up.js
@@ -1,4 +1,4 @@
-"use strict"
+// "use strict"
 
 var buttonUp = document.querySelector('.button-up');
 var scrollHeight = document.documentElement.scrollHeight;
--- source/js/toggle.js
+++ source/js/toggle.js
@@ -1,4 +1,4 @@
-"use strict"
+// "use strict"
 
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.page-header__toggle');
